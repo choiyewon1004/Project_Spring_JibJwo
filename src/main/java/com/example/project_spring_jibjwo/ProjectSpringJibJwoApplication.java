e220b0c8e0bff8e50dbb808a5a02c3a64f039b92
choiyewon <choiyewon1997@gmail.com>
2023-11-06 16:12:55 +0900

    Initial commit

---
+++ src/main/java/com/example/project_spring_jibjwo/ProjectSpringJibJwoApplication.java
@@ -0,0 +1,13 @@
+package com.example.project_spring_jibjwo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectSpringJibJwoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectSpringJibJwoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/project_spring_jibjwo/ProjectSpringJibJwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.project_spring_jibjwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectSpringJibJwoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
